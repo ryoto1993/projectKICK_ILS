5654785155c99c2b924a8652f2091d001b6f158a
Ryoto Tomioka <tommy.ryoto93@gmail.com>
2017-02-10 02:25:07 +0900

    tiny progress on UI

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -10,12 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("mainTab.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("Intelligent Lighting System Controller");
-        primaryStage.setScene(new Scene(root, 1600, 1000));
+        primaryStage.setScene(new Scene(root, 900, 580));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
